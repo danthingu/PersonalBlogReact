b89830fb1e5a549ae8d0a3b0dd89e5c6b6309fb4
Thien (Daniel) Nguyen <danielnguyen@microvu.com>
2018-03-05 18:41:56 -0800

    edit post_index file

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount() { // this component function will automicatlly called by react immediately after this component has shown up inside the DOM
+        this.props.fetchPosts();
+    }
     render() {
+        console.log("here is : " + this.props.posts);
         return ( 
             <div>
                 PostsIndex
@@ -10,4 +16,10 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+
+function mapStateToProps(state) {
+    return {posts: state.posts}
+}
+
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex); // this is shortcut instead of using mapDispatcherToProps
